a4c09c449badec7d64ffb838c58ca9adfb75a411
GamzeUnal <u.gamze0@gmail.com>
2022-08-20 13:45:38 +0300

    initial commit

--- shortener/shortener/settings.py
+++ shortener/shortener/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yl*7-$lrn48r29jcxubyh!w@nfm=(b#b_9f&ks-jgz5djy1&*n'
+SECRET_KEY = 'django-insecure-y*oev#ru&h2vnv=v2tq)_x2m_15rh4r&qhqsdy==&8-2q^f)u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
